2c02c2f19c14ef1a9efacd9c76b5d8ee76499186
Mani246-sido <maninder13905@gmail.com>
2026-09-01 15:55:56 +0530

    feat: update security configuration to enhance authorization and exception handling

--- backend/src/main/java/com/repoMind/backend/config/securityConfig.java
+++ backend/src/main/java/com/repoMind/backend/config/securityConfig.java
@@ -2,11 +2,14 @@ package com.repoMind.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +24,19 @@ public class securityConfig {
             .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+            .authorizeHttpRequests(auth->auth.requestMatchers(
+                "/api/auth/login-url",
+                "/oauth2/**",
+                "/login/oauth2/**",
+                "/error"
+            )
+        .permitAll()
+    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+.requestMatchers("/api/**").authenticated()
+    .anyRequest().permitAll())
+    .exceptionHandling(ex->ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+
+            
     }
 
 
